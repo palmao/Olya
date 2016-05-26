1b32566ad9ec506a35156bb79458cc5d2951c2b5
palmao <palma.o@mail.ru>
2016-05-26 18:49:48 +0300

    Seminar1

---
+++ sem01/src/main/java/sem01/sem1.java
@@ -0,0 +1,15 @@
+package sem01;
+
+/**
+ * Hello world!
+ *
+ */
+public class sem1 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println();
+        
+    }
+}
